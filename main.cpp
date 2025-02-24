ea9b1f2c9ee869e537628595b9da6e9234354b9e
nrleist <nrleist@gmail.com>
2025-02-24 15:47:15 -0500

    Add delarations for motors, servos, sensors

--- main.cpp
+++ main.cpp
@@ -1,95 +1,109 @@
 #include <FEHLCD.h>
 #include <FEHIO.h>
 #include <FEHUtility.h>
+#include <FEHSD.h>
+#include <FEHBattery.h>
+#include <FEHBuzzer.h>
 #include <FEHMotor.h>
+#include <FEHServo.h>
+
 #include <cmath>
+#include <string.h>
+#include <iostream>
+using namespace std;
 
-//Declarations for encoders & motors // TODO: Get correct ports
-DigitalEncoder right_encoder(FEHIO::P0_0);
-DigitalEncoder left_encoder(FEHIO::P3_0);
-FEHMotor right_motor(FEHMotor::Motor1,9.0);
-FEHMotor left_motor(FEHMotor::Motor0,9.0);
+//Declarations for encoders & motors 
+DigitalEncoder leftEncoder(FEHIO::P0_0);
+DigitalEncoder rightEncoder(FEHIO::P3_7);
+FEHMotor leftMotor(FEHMotor::Motor0, 9.0);
+FEHMotor rightMotor(FEHMotor::Motor1, 9.0);
+FEHMotor spinner(FEHMotor::Motor2, 5.0);  // TODO: Check max volts for CRS
 
-// TODO: Add sensor declarations
+// Declarations for servos
+FEHServo armServo(FEHServo::Servo0);
+FEHServo hookServo(FEHServo::Servo1);
+
+// Declarations for sensors
+AnalogInputPin leftOpto(FEHIO::P1_0);
+AnalogInputPin centerOpto(FEHIO::P1_0);
+AnalogInputPin rightOpto(FEHIO::P1_0);
+AnalogInputPin lightSenor(FEHIO::P1_4);
+
+// Values to reverse motors
+const int leftReverse = 0;
+const int rightReverse = 0;
+const int spinReverse = 0;
 
 // TODO: Get drivetrain width and test counts.
 #define COUNTS_PER_INCH_RIGHT 43
 #define COUNTS_PER_INCH_LEFT 43
 #define DRIVETRAIN_WIDTH 7
 
-#define PI 3.1459
+#define PI 3.1415926
 
 // Function declarations here
-void wait_for_touch();
-void move_forward(int percent, int inches);
+void waitForTouch(string prgName);
+void moveForward(int percent, int inches);
 void turn(int percent, int direction, int degrees);
 
 int main(void)
 {
-    float x, y; //for touch screen
+    waitForTouch("Milestone 01");
 
-    //Initialize the screen
-    LCD.Clear(BLACK);
-    LCD.SetFontColor(WHITE);
-
-    LCD.WriteLine("Encoder Nav");
-    LCD.WriteLine("Touch the screen");
-    while(!LCD.Touch(&x,&y)); //Wait for screen to be pressed
-    while(LCD.Touch(&x,&y)); //Wait for screen to be unpressed
-
-    move_forward(30, 14);
-    Sleep(50);
-    turn(20, -1, 90);
-    Sleep(50);
-    move_forward(30, 10);
-    Sleep(50);
-    turn(20, 1, 90);
-    Sleep(50);
-    move_forward(20, 4);
-    Sleep(50);
+    // TODO: Write Steps for Milestone 1
 }
 
 // Function definitions here
-void waitForTouch() {
+void waitForTouch(char prgName[]) {
     float x, y;
+
+    LCD.Clear(BLACK);
+    LCD.SetFontColor(WHITE);
+    LCD.WriteLine(prgName);
+    LCD.WriteLine("Touch the screen");
+
     while(!LCD.Touch(&x,&y)); //Wait for screen to be pressed
     while(LCD.Touch(&x,&y)); //Wait for screen to be unpressed
+
+    LCD.Clear();
+    LCD.WriteLine(prgName);
+    LCD.WriteLine("Running...");
 }
 
 
-void move_forward(int percent, int inches) {
+void moveForward(int percent, int inches) {
     //Reset encoder counts
-    right_encoder.ResetCounts();
-    left_encoder.ResetCounts();
+    leftEncoder.ResetCounts();
+    rightEncoder.ResetCounts();
 
     //Set both motors to desired percent
-    right_motor.SetPercent(percent);
-    left_motor.SetPercent(percent);
-
+    leftMotor.SetPercent(percent * leftReverse);
+    rightMotor.SetPercent(percent * rightReverse);
+    
     //While the average of the left and right encoder is less than counts,
     //keep running motors
-    while((left_encoder.Counts() + right_encoder.Counts()) / 2. < inches * COUNTS_PER_INCH_RIGHT);
+    while((leftEncoder.Counts() + rightEncoder.Counts()) / 2. < inches * COUNTS_PER_INCH_RIGHT);
 
     //Turn off motors
-    right_motor.Stop();
-    left_motor.Stop();
+    rightMotor.Stop();
+    leftMotor.Stop();
 }
 
 void turn(int percent, int direction, int degrees) {
     int turnInches = float((PI * DRIVETRAIN_WIDTH) * (degrees / 360.0));
-    LCD.WriteLine("Turning");
+    //LCD.WriteLine("Turning");
 
-    right_encoder.ResetCounts();
-    left_encoder.ResetCounts();
+    leftEncoder.ResetCounts();
+    rightEncoder.ResetCounts();
 
-    right_motor.SetPercent(percent * -direction);
-    left_motor.SetPercent(percent * direction);
+    leftMotor.SetPercent(percent * direction * leftReverse);
+    rightMotor.SetPercent(percent * -direction * rightReverse);
 
     //While the average of the left and right encoder is less than counts,
     //keep running motors
-    while(((abs(left_encoder.Counts()) + abs(right_encoder.Counts())) / 2.0) < turnInches * COUNTS_PER_INCH_LEFT);
+    while(((abs(leftEncoder.Counts()) + abs(rightEncoder.Counts())) / 2.0) < turnInches * COUNTS_PER_INCH_LEFT);
 
     //Turn off motors
-    right_motor.Stop();
-    left_motor.Stop();
+    rightMotor.Stop();
+    leftMotor.Stop();
 }
